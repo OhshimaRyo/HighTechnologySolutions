134e71dbffeaf825ae82ddb0d6e632b1d0e61dba
ryoohshima <ryo.ohshima@actdesign-lab.co.jp>
2021-10-17 16:35:33 +0900

    init push

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <header>
+      <a>ホーム</a>
+      <a>会社概要</a>
+      <a>サービス</a>
+      <a>制作実績</a>
+      <a>記事メディア</a>
+      <a>お問い合わせ</a>
+    </header>
